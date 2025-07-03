eee6caaa6b45d4a0c4a04e2bd5f214ad86872266
Victor Tanedo <victortanedo@outlook.com>
2025-07-03 10:58:32 +0800

    Updated: Hello Anflocor

--- MyFirstGitRepo/Program.cs
+++ MyFirstGitRepo/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.ForegroundColor = ConsoleColor.Cyan;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Anflocor!");
 
