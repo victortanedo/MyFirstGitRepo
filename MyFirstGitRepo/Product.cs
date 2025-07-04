b107392da046f4e57b4af7c4393d1a5526f0f775
Victor Tanedo <victortanedo@outlook.com>
2025-07-04 14:37:47 +0800

    Updated: Product class

--- MyFirstGitRepo/Product.cs
+++ MyFirstGitRepo/Product.cs
@@ -2,10 +2,8 @@
 
 public class Product
 {
-	public Product()
-	{
-		public string Name { get; set; }
-		public decimal Price { get; set; }
-		public DateTime CreatedAt { get; set; }
-	}
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
+
